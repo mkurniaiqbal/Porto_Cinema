bf7d13004020339af70c7cebf396dd9ace29ad15
Muhammad Kurnia Iqbal <mkurniaiqbal99@gmail.com>
2024-09-13 09:43:48 +0700

    menghandle ketika tidak ada film

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -33,60 +33,66 @@ function Card({ films, headerCard, className, className2 }) {
             <h4>{headerCard}</h4>
           </div>
 
-          {films.map((film) => (
-            <div
-              className="col-6 col-sm-4 col-md-3 col-lg-2 py-2"
-              key={film.id}
-            >
+          {films.length == 0 ? (
+            <div className="text-center">
+              <h5>No films found ..</h5>
+            </div>
+          ) : (
+            films.map((film) => (
               <div
-                className="card border-0 rounded size-card"
-                data-aos="fade-up"
+                className="col-6 col-sm-4 col-md-3 col-lg-2 py-2"
+                key={film.id}
               >
-                <Link
-                  to={`/detail/${film.id}`}
-                  state={{ film }}
-                  className="text-decoration-none"
+                <div
+                  className="card border-0 rounded size-card"
+                  data-aos="fade-up"
                 >
+                  <Link
+                    to={`/detail/${film.id}`}
+                    state={{ film }}
+                    className="text-decoration-none"
+                  >
+                    <div className="position-relative">
+                      <img
+                        src={
+                          film.poster_path
+                            ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
+                            : `https://i.pinimg.com/736x/5e/e5/b8/5ee5b85fa75816d3f9a1f4e2f138a89c.jpg`
+                        }
+                        className="card-img-top size-img-card"
+                        alt={film.title}
+                      />
+                    </div>
+                    <div className="card-body bg-card rounded-bottom">
+                      <h5 className="text-header-card text-ellipsis">
+                        {film.title ? film.title : film.name}
+                      </h5>
+                      <p>
+                        {film.release_date
+                          ? film.release_date.split("-")[0]
+                          : film.first_air_date.split("-")[0]}
+                      </p>
+                    </div>
+                  </Link>
+
                   <div className="position-relative">
                     <img
-                      src={
-                        film.poster_path
-                          ? `https://image.tmdb.org/t/p/w500${film.poster_path}`
-                          : `https://i.pinimg.com/736x/5e/e5/b8/5ee5b85fa75816d3f9a1f4e2f138a89c.jpg`
-                      }
-                      className="card-img-top size-img-card"
-                      alt={film.title}
+                      src={Mark}
+                      alt="Mark Icon"
+                      className="icon-mark"
+                      onClick={() => handleMark(film)}
+                    />
+                    <img
+                      src={Favorite}
+                      alt="Favorite Icon"
+                      className="icon-favorite"
+                      onClick={() => handleFavorite(film)}
                     />
                   </div>
-                  <div className="card-body bg-card rounded-bottom">
-                    <h5 className="text-header-card text-ellipsis">
-                      {film.title ? film.title : film.name}
-                    </h5>
-                    <p>
-                      {film.release_date
-                        ? film.release_date.split("-")[0]
-                        : film.first_air_date.split("-")[0]}
-                    </p>
-                  </div>
-                </Link>
-
-                <div className="position-relative">
-                  <img
-                    src={Mark}
-                    alt="Mark Icon"
-                    className="icon-mark"
-                    onClick={() => handleMark(film)}
-                  />
-                  <img
-                    src={Favorite}
-                    alt="Favorite Icon"
-                    className="icon-favorite"
-                    onClick={() => handleFavorite(film)}
-                  />
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </div>
